0a65e9adf8e1ca9f3f5bce34cb17f3e0cd8ac726
Pervansh <57684612+Pervansh@users.noreply.github.com>
2022-03-01 16:11:25 +0300

    Delete RobotrafficMachine/RobotrafficMachine directory

--- RobotrafficMachine/RobotrafficMachine/CarStatistics.cpp
+++
@@ -1,8 +0,0 @@
-#include "CarStatistics.h"
-#include "CarBehavior.h"
-
-int CarStatistics::avarageLineError = 0;
-
-void CarStatistics::update(CarBehavior* behavior, CarBehavior::ListnerType type) {
-
-}
--- RobotrafficMachine/RobotrafficMachine/EmptyBehavior.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "CarBehavior.h"
-
-class EmptyBehavior : public CarBehavior {
-public:
-    EmptyBehavior() : CarBehavior(0, 4, 8) {}
-};
